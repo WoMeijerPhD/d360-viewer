2accf1aec60cd991d32db449f0ffa8f8e8f55f27
Wo Meijer <79914012+WoMeijerPhD@users.noreply.github.com>
2024-01-11 09:07:26 +0100

    started adding support for miro sticky note covers

--- src/components/helper-functions.js
+++ src/components/helper-functions.js
@@ -11,6 +11,43 @@ export const randomColor = () => {
     return colors[Math.floor(Math.random() * colors.length)];
 }
 
+const miro2html = {
+    "gray":"gray",
+    "light_yellow":"LightYellow",
+    "yellow":"yellow",
+    "orange":"orange",
+    "light_green":"LightGreen",
+    "green":"green",
+    "dark_green":"DarkGreen",
+    "cyan":"cyan",
+    "light_pink":"LightPink",
+    "pink":"pink",
+    "violet":"violet",
+    "red":"red",
+    "light_blue":"LightBlue",
+    "blue":"blue",
+    "dark_blue":"Dark_Blue",
+    "black":"black"
+};
+
+/** takes the miro color name and returns the html color */
+export const miroColor2html = (miroColor) => {
+    // check if the color is in the list
+    if (!miro2html.hasOwnProperty(miroColor)) {
+        return "LightYellow";
+    }
+    return miro2html[miroColor];
+}
+
+/** takes the html color name and returns the miro color */
+export const htmlColor2miro = (htmlColor) => {
+    for (let [key, value] of Object.entries(miro2html)) {
+        if (value === htmlColor) {
+            return key;
+        }
+    }
+}
+
 
 // export const randomColor = () => {
 //     return "#" + Math.floor(Math.random() * 16777215).toString(16);
